39e04a6d20732d952906af5a54530891daeff48e
spike <ololmkbc.dev@gmail.com>
2024-02-12 22:49:17 +0200

    feat: add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import logo1 from './jameson.png';
+import logo2 from './mersedes.png';
+import logo3 from './metro.png';
+import logo4 from './karher.png';
+import logo5 from './socar.png';
+import logo6 from './toyota.png';
+
+import logo from './logo.svg';
+
+export class Assets {
+  static logos = [logo1, logo2, logo3, logo4, logo5, logo6];
+  static logo = logo;
+}
